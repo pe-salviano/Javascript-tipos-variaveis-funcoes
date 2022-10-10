2e9811467942d365a7c65ca90a7b1ec352a9f5ad
pe-salviano <pedro_s.c@hotmail.com>
2022-10-10 12:16:58 -0300

    Tipos

---
+++ type-string.js
@@ -0,0 +1,10 @@
+const texto1 = "Olá, mundo!";
+const texyo2 = 'Olá, mundo!';
+const senha = "senhaSegura123";
+const stringDeNumeros = "1235";
+
+const citacao = "O leo disse 'Oi!'";
+
+//ou
+
+const citacao2 ='O leo disse "Oi!';
